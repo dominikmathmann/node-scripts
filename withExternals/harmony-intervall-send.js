80fea57f67b2e14a4733c85b75869f4be6999939
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2019-11-18 16:33:36 +0100

    Logitech Harmony Hub Kommando senden

---
+++ withExternals/harmony-intervall-send.js
@@ -0,0 +1,20 @@
+var harmony = require("harmonyhubjs-client");
+
+harmony("192.168.178.26").then(function(harmonyClient) {
+  harmonyClient.getAvailableCommands().then(function(commands) {
+    var device = commands.device[1];
+    var powerControls = device.controlGroup
+      .filter(function(group) {
+        return group.name.toLowerCase() === "displaymode";
+      })
+      .pop();
+    var functionToCall = powerControls["function"][0];
+
+    if (functionToCall) {
+      var encodedAction = functionToCall.action.replace(/\:/g, "::");
+      return harmonyClient.send("holdAction", "action=" + encodedAction + ":status=press");
+    } else {
+      throw new Error("Cant fire command.");
+    }
+  });
+});
